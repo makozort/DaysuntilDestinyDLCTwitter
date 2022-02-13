8d04d94c1fa76e5f56ccb72452e9393591d21f4c
Makozort/Jack M <67140936+makozort@users.noreply.github.com>
2022-02-13 23:22:50 +1100

    cleaned up

--- timeuntilbot.py
+++ timeuntilbot.py
@@ -17,8 +17,8 @@ with open(f"./keys.json", 'r') as f:
 
 
 
-    
-client = tweepy.Client( 
+
+client = tweepy.Client(
     consumer_key=consumerkey,
     consumer_secret=consumersecret,
     access_token=accesstoken,               # pass variables into something the client class can use
@@ -26,26 +26,29 @@ client = tweepy.Client(
 )
 
 today = datetime.today()
-dlcdate = datetime(day=22, month=2, year=2022)
+dlcdate = datetime(day=23, month=2, year=2022)
 dlcname = " The Witch Queen"
+
+
+if today.date() == dlcdate.date():
+    text = (dlcname + " is out!")
+    client.create_tweet(text=text)
+    exit()
 
 i = str(dlcdate - today) # get the time between now release of dlc
 if "day" in i:    # look, it was 5am and this solved 2 problems I had
-    difference = (int(i.split(" ")[0]) + 1) # and add 1 to it because it does not count the date its trying to reach as a full day
+    difference = (int(i.split(" ")[0]))
 else:
-    text = ("1 day until" + dlcname)
+    text = ("1 reset until" + dlcname)
     client.create_tweet(text=text) # python is weird how it does datetime imo, or maybe im just dumb. either way the workaround to some issue.. well, works.
     exit()
 
-
-if difference == 0: #if the dlc is out
-    text = (dlcname + " is out!")
-elif difference < 0: # don't send a tweet if the dlc is already out
-    print("dlc already out") 
+if difference < 0: # don't send a tweet if the dlc is already out
+    print("dlc already out")
     exit()
 else:
     text = (str(difference) + " days until" + dlcname)
-    
+
 client.create_tweet(text=text) # send the tweet!
 
 
